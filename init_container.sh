6206fa8da287102220c80eb882e4f8c55426cc8e
hongbin <ying.hongbin.1016@gmail.com>
2026-01-20 17:47:46 +0800

    feat: using mount volume as home dir

--- init_container.sh
+++ init_container.sh
@@ -11,12 +11,8 @@ if [ -z "$NEW_PWD" ]; then
 fi
 
 # Create a sudo user account with a password and a /bin/bash shell
-useradd -m -G sudo -s /bin/bash $NEW_USER &>/dev/null
+useradd -m -d /data -G sudo -s /bin/bash $NEW_USER &>/dev/null
 echo "$NEW_USER:$NEW_PWD" | chpasswd
-
-echo ". /opt/conda/etc/profile.d/conda.sh" >> /home/$NEW_USER/.bashrc
-echo "conda activate base" >> /home/$NEW_USER/.bashrc
-chown "$NEW_USER:$NEW_USER" /home/$NEW_USER/.bashrc
 
 # Start SSH service
 service ssh start
